6185617dadc4834406a2065824679b11d2607868
MiloslavStekrt <stekrt18@gmail.com>
2020-08-23 20:36:42 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My master XD</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="name">Napište své jmeno</label>
+        <input type="text" name="name" id="name">
+        <label for="num_dress">Číslo dressu</label>
+        <input type="number" name="num_dress" id="num_dress">
+        <button type="submit">Přidat hrače</button>
+    </form>
+    <form action="" method="post">
+        <label for="name">Jmeno zapasu</label>
+        <input type="text" name="name" id="name">
+    </form>
+</body>
+</html>
